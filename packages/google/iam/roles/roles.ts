a0851fee28ef27995bc289d8711aeafca9b57c69
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2023-06-15 07:57:33 +1000

    feat(google-iam): added pubsub publisher role

--- packages/google/iam/roles/roles.ts
+++ packages/google/iam/roles/roles.ts
@@ -1,12 +1,16 @@
-export enum StorageRoles {
-  OBJECT_ADMIN = "roles/storage.objectAdmin",
-  OBJECT_CREATOR = "roles/storage.objectCreator",
-  OBJECT_VIEWER = "roles/storage.objectViewer",
-}
-
 export enum CloudKMSRoles {
   ADMIN = "roles/cloudkms.admin",
   ENCRYPTER = "roles/cloudkms.cryptoKeyEncrypter",
   ENCRYPTER_DECRYPTER = "roles/cloudkms.cryptoKeyEncrypterDecrypter",
   VIEWER = "roles/cloudkms.viewer",
+}
+
+export enum PubSubRoles {
+  PUBLISHER = "roles/pubsub.publisher",
+}
+
+export enum StorageRoles {
+  OBJECT_ADMIN = "roles/storage.objectAdmin",
+  OBJECT_CREATOR = "roles/storage.objectCreator",
+  OBJECT_VIEWER = "roles/storage.objectViewer",
 }

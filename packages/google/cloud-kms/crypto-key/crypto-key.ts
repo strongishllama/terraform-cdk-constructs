de62403085ce9deb7411d06c8df62d520fda7137
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2023-06-15 07:55:24 +1000

    feat(google-cloud-kms): added random suffix to CryptoKey name

--- packages/google/cloud-kms/crypto-key/crypto-key.ts
+++ packages/google/cloud-kms/crypto-key/crypto-key.ts
@@ -1,5 +1,6 @@
 import { Construct } from "constructs";
 import { kmsCryptoKey, kmsCryptoKeyIamMember } from "@cdktf/provider-google";
+import { id as _id } from "@cdktf/provider-random";
 import { IKeyRing } from "../key-ring/key-ring";
 import { GrantConfig, IGrantable } from "@terraform-cdk-constructs/google-iam";
 import { CloudKMSRoles } from "@terraform-cdk-constructs/google-iam";
@@ -7,9 +8,6 @@ import { CloudKMSRoles } from "@terraform-cdk-constructs/google-iam";
 export interface CryptoKeyConfig {
   readonly keyRing: IKeyRing;
   readonly name: string;
-  readonly labels?: {
-    [key: string]: string;
-  };
 }
 
 export class CryptoKey extends Construct {
@@ -18,10 +16,12 @@ export class CryptoKey extends Construct {
   constructor(scope: Construct, id: string, config: CryptoKeyConfig) {
     super(scope, id);
 
+    const suffix = new _id.Id(this, "suffix", {
+      byteLength: 6,
+    });
     this.resource = new kmsCryptoKey.KmsCryptoKey(this, "resource", {
       keyRing: config.keyRing.id,
-      name: config.name,
-      labels: config.labels,
+      name: `${config.name}-${suffix.id}`,
     });
   }
 
@@ -33,29 +33,29 @@ export class CryptoKey extends Construct {
     return this.resource.id;
   }
 
-  public grantAdmin(grantee: IGrantable): void {
+  public grantAdmin(grantee: IGrantable): kmsCryptoKeyIamMember.KmsCryptoKeyIamMember {
     return this.grant(grantee, {
       id: this.resource.id,
       role: CloudKMSRoles.ADMIN,
     });
   }
 
-  public grantEncrypterDecrypter(grantee: IGrantable): void {
+  public grantEncrypterDecrypter(grantee: IGrantable): kmsCryptoKeyIamMember.KmsCryptoKeyIamMember {
     return this.grant(grantee, {
       id: this.resource.id,
       role: CloudKMSRoles.ENCRYPTER_DECRYPTER,
     });
   }
 
-  public grantViewer(grantee: IGrantable): void {
+  public grantViewer(grantee: IGrantable): kmsCryptoKeyIamMember.KmsCryptoKeyIamMember {
     return this.grant(grantee, {
       id: this.resource.id,
       role: CloudKMSRoles.VIEWER,
     });
   }
 
-  private grant(grantee: IGrantable, config: GrantConfig): void {
-    new kmsCryptoKeyIamMember.KmsCryptoKeyIamMember(this, "member", {
+  private grant(grantee: IGrantable, config: GrantConfig): kmsCryptoKeyIamMember.KmsCryptoKeyIamMember {
+    return new kmsCryptoKeyIamMember.KmsCryptoKeyIamMember(this, "member", {
       cryptoKeyId: config.id,
       member: grantee.grantMember,
       role: config.role,

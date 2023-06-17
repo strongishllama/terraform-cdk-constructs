d41683a1e706f546464b89675b340c5ed3e4f7dc
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2023-06-17 15:39:40 +1000

    feat(google-artifact-registry): added docker asset support

--- packages/google/artifact-registry/repository/repository.ts
+++ packages/google/artifact-registry/repository/repository.ts
@@ -1,8 +1,11 @@
 import { Construct } from "constructs";
-import { artifactRegistryRepository, artifactRegistryRepositoryIamMember } from "@cdktf/provider-google";
+import { artifactRegistryRepository, artifactRegistryRepositoryIamMember, kmsCryptoKeyIamMember } from "@cdktf/provider-google";
 import { RepositoryFormat } from "../repository-format";
 import { CryptoKey } from "@terraform-cdk-constructs/google-cloud-kms";
 import { ArtifactRegistryRoles, GrantConfig, IGrantable } from "@terraform-cdk-constructs/google-iam";
+import { ServiceAgent } from "../service-agent";
+import { IProject, Project } from "@terraform-cdk-constructs/google-project";
+import { DockerImage } from "@terraform-cdk-constructs/assets";
 
 export interface RepositoryConfig {
   readonly format: RepositoryFormat;
@@ -12,45 +15,78 @@ export interface RepositoryConfig {
 
 export class Repository extends Construct {
   private readonly resource: artifactRegistryRepository.ArtifactRegistryRepository;
+  private readonly project: IProject;
+  private readonly format: RepositoryFormat;
   private readonly cryptoKey?: CryptoKey;
 
   constructor(scope: Construct, id: string, config: RepositoryConfig) {
     super(scope, id);
 
+    this.project = Project.fromProjectAttributes(this, "project");
+    this.format = config.format;
+
+    let grant: kmsCryptoKeyIamMember.KmsCryptoKeyIamMember | undefined = undefined;
+
     this.cryptoKey = config.cryptoKey;
+    if (this.cryptoKey) {
+      const serviceAgent = new ServiceAgent(this, "service-agent", {
+        projectNumber: this.project.number,
+      });
+      grant = this.cryptoKey.grantEncrypterDecrypter(`${id}-service-agent`, serviceAgent);
+    }
 
     this.resource = new artifactRegistryRepository.ArtifactRegistryRepository(this, "resource", {
-      format: config.format,
-      kmsKeyName: config.cryptoKey?.name,
+      format: this.format,
+      kmsKeyName: config.cryptoKey?.id,
       repositoryId: config.name,
+      dependsOn: grant ? [grant] : undefined,
     });
   }
 
-  public grantAdmin(grantee: IGrantable): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
-    return this.grant(grantee, {
-      id: this.resource.name,
-      role: ArtifactRegistryRoles.REPOSITORY_ADMIN,
-    });
+  public get id(): string {
+    return this.resource.id;
   }
 
-  public grantReader(grantee: IGrantable): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
-    return this.grant(grantee, {
-      id: this.resource.name,
-      role: ArtifactRegistryRoles.REPOSITORY_ADMIN,
-    });
+  public get name(): string {
+    return this.resource.name;
   }
 
-  public grantWriter(grantee: IGrantable): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
-    return this.grant(grantee, {
-      id: this.resource.name,
-      role: ArtifactRegistryRoles.REPOSITORY_ADMIN,
-    });
-  }
-
-  private grant(grantee: IGrantable, config: GrantConfig): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
-    if (this.cryptoKey !== undefined) {
-      this.cryptoKey.grantEncrypterDecrypter(grantee);
+  public addAsset(id: string, config: AssetConfig): void {
+    switch (this.format) {
+      case RepositoryFormat.DOCKER:
+        new DockerImage(this, id, {
+          path: config.path,
+          name: `${this.resource.location}-docker.pkg.dev/${this.project.name}/${this.resource.name}/${config.name}`,
+        });
+        break;
+      default:
+        throw new Error(`google-artifact-registry.Repository construct does not yet support the adding of ${this.resource.format} format yet.`);
     }
+  }
+
+  public grantAdmin(id: string, grantee: IGrantable): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
+    return this.grant(id, grantee, {
+      id: this.resource.name,
+      role: ArtifactRegistryRoles.REPOSITORY_ADMIN,
+    });
+  }
+
+  public grantReader(id: string, grantee: IGrantable): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
+    return this.grant(id, grantee, {
+      id: this.resource.name,
+      role: ArtifactRegistryRoles.REPOSITORY_ADMIN,
+    });
+  }
+
+  public grantWriter(id: string, grantee: IGrantable): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
+    return this.grant(id, grantee, {
+      id: this.resource.name,
+      role: ArtifactRegistryRoles.REPOSITORY_ADMIN,
+    });
+  }
+
+  private grant(id: string, grantee: IGrantable, config: GrantConfig): artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember {
+    this.cryptoKey?.grantEncrypterDecrypter(id, grantee);
 
     return new artifactRegistryRepositoryIamMember.ArtifactRegistryRepositoryIamMember(this, "member", {
       member: grantee.grantMember,
@@ -58,4 +94,9 @@ export class Repository extends Construct {
       role: config.role,
     });
   }
+}
+
+export interface AssetConfig {
+  readonly path: string;
+  readonly name: string;
 }

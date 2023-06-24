cd263639eb74431debde1a9d2f5f306c1c1f99ae
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2023-06-24 20:01:47 +1000

    feat(google-compute-engine): added default service account

--- packages/google/compute-engine/index.ts
+++ packages/google/compute-engine/index.ts
@@ -1,2 +1,3 @@
 export * from "./region";
+export * from "./service-account";
 export * from "./zone";
---
+++ packages/google/compute-engine/service-account/service-account.ts
@@ -0,0 +1,15 @@
+import { Construct } from "constructs";
+
+export interface ServiceAccountConfig {
+  readonly projectNumber: string;
+}
+
+export class ServiceAccount extends Construct {
+  public readonly grantMember: string;
+
+  constructor(scope: Construct, id: string, config: ServiceAccountConfig) {
+    super(scope, id);
+
+    this.grantMember = `serviceAccount:${config.projectNumber}-compute@developer.gserviceaccount.com`;
+  }
+}

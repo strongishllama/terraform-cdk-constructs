d5d56ba038c9bb4139dc67cd70a6b332a630e703
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2023-06-28 09:13:19 +1000

    feat(google-eventarc): added destination and matching criteria support to tigger construct

---
+++ packages/google/eventarc/trigger-type/trigger-type.ts
@@ -0,0 +1,3 @@
+export enum TriggerType {
+  CLOUD_STORAGE_OBJECT_V1_FINALIZED = "google.cloud.storage.object.v1.finalized",
+}
--- packages/google/eventarc/trigger/trigger.ts
+++ packages/google/eventarc/trigger/trigger.ts
@@ -1,31 +1,43 @@
 import { Construct } from "constructs";
 import { eventarcTrigger } from "@cdktf/provider-google";
 import { Region } from "@terraform-cdk-constructs/google-compute-engine";
+import * as destinations from "@terraform-cdk-constructs/google-eventarc-destinations";
 
 export interface TriggerConfig {
-  readonly location: Region;
   readonly name: string;
+  readonly location: Region;
+  readonly destination: destinations.ITriggerDestination;
+  readonly matchingCriteria: MatchingCriteria[];
 }
 
 export class Trigger extends Construct {
-  private readonly resource: eventarcTrigger.EventarcTrigger;
+  // private readonly resource: eventarcTrigger.EventarcTrigger;
 
   constructor(scope: Construct, id: string, config: TriggerConfig) {
     super(scope, id);
 
-    this.resource = new eventarcTrigger.EventarcTrigger(this, "resource", {
+    let cloudRunService: eventarcTrigger.EventarcTriggerDestinationCloudRunService | undefined;
+    if (config.destination.config.cloudRunService) {
+      cloudRunService = {
+        path: config.destination.config.cloudRunService.path,
+        region: config.destination.config.cloudRunService.service.location,
+        service: config.destination.config.cloudRunService.service.id,
+      };
+    }
+
+    new eventarcTrigger.EventarcTrigger(this, "resource", {
       location: config.location,
       name: config.name,
-      destination: {},
-      matchingCriteria: [
-        {
-          attribute: "",
-          value: "",
-        },
-      ],
+      destination: {
+        workflow: config.destination.config.workflow?.id,
+        cloudRunService: cloudRunService,
+      },
+      matchingCriteria: config.matchingCriteria,
     });
-
-    if (this.resource) {
-    }
   }
+}
+
+export interface MatchingCriteria {
+  readonly attribute: string;
+  readonly value: string;
 }

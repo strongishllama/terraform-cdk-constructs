105a9001f47e597de31ccb22935d5c10765df2ed
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2023-06-28 09:15:39 +1000

    feat(google-kubernetes-engine): added private cluster support

--- packages/google/kubernetes-engine/cluster/cluster.ts
+++ packages/google/kubernetes-engine/cluster/cluster.ts
@@ -1,28 +1,33 @@
 import { Construct } from "constructs";
 import { containerCluster, containerNodePool } from "@cdktf/provider-google";
-import { Region, ServiceAccount } from "@terraform-cdk-constructs/google-compute-engine";
+import { ISubnet, IVpc, Region, ServiceAccount } from "@terraform-cdk-constructs/google-compute-engine";
 import { IProject, Project } from "@terraform-cdk-constructs/google-project";
 import { ClusterMode } from "../cluster-mode";
+import { ClusterAccess } from "../cluster-access";
 
 export interface ClusterConfig {
   readonly name: string;
-  readonly region: Region;
-  readonly nodes: number;
   readonly mode: ClusterMode;
+  readonly access: ClusterAccess;
+  readonly nodes: number;
+  readonly region: Region;
+  readonly vpc?: IVpc;
+  readonly subnet?: ISubnet;
 }
 
 export class Cluster extends Construct {
   public readonly grantMember: string;
 
-  private readonly resource: containerCluster.ContainerCluster;
   protected readonly project: IProject;
+
+  private readonly resource: containerCluster.ContainerCluster;
 
   constructor(scope: Construct, id: string, config: ClusterConfig) {
     super(scope, id);
 
     this.project = Project.fromProjectAttributes(this, "project");
     this.grantMember = new ServiceAccount(this, "compute-engine-service-account", {
-      projectNumber: Project.fromProjectAttributes(this, "project").number,
+      projectNumber: this.project.number,
     }).grantMember;
 
     // Workaround for this bug: https://github.com/hashicorp/terraform-provider-google/issues/10782.
@@ -34,13 +39,24 @@ export class Cluster extends Construct {
       };
     }
 
+    let privateClusterConfig: containerCluster.ContainerClusterPrivateClusterConfig | undefined;
+    if (config.access === ClusterAccess.PRIVATE) {
+      privateClusterConfig = {
+        enablePrivateEndpoint: true,
+        enablePrivateNodes: true,
+      };
+    }
+
     this.resource = new containerCluster.ContainerCluster(this, "cluster-resource", {
       enableAutopilot: config.mode === ClusterMode.AUTOPILOT,
       initialNodeCount: config.mode === ClusterMode.AUTOPILOT ? undefined : 1,
       location: config.region,
       name: config.name,
+      network: config.vpc?.id,
       removeDefaultNodePool: config.mode === ClusterMode.AUTOPILOT ? undefined : true,
+      subnetwork: config.subnet?.id,
       ipAllocationPolicy: ipAllocationPolicy,
+      privateClusterConfig: privateClusterConfig,
     });
 
     if (config.mode === ClusterMode.STANDARD) {
